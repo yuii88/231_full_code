8613ec1620c95d6ba75a3359ef96535210de5571
Saiya9 <firsttomany097@gmail.com>
2021-11-15 16:49:56 +0700

    register

--- Django 50 Hrs/firstweb/myapp/urls.py
+++ Django 50 Hrs/firstweb/myapp/urls.py
@@ -1,9 +1,10 @@
 #urls.py
 from django.urls import path, include 
-from .views import Home, About, Contact, Flightstatus
+from .views import *
 urlpatterns = [
 	path('', Home, name='home-page'),
 	path('about', About, name='about-page'),
 	path('contact', Contact, name='contact-page'),
 	path('flightstatus', Flightstatus, name='flightstatus-page'),
+	path('register/',Register, name='register-page'),
 ]
--- Django 50 Hrs/firstweb/myapp/views.py
+++ Django 50 Hrs/firstweb/myapp/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 #from django.http import HttpResponse โชข้อความหน้าเว็บ
  
 def Home(request):
 	product1 = 'register'
 	product2 = 'Booking'
-	product3 = 'AboutUs'
+	product3 = 'AboutUs' 
 
 	context = {'product1':product1,'product2':product2,'product3':product3}
 	return render(request,'myapp/home.html',context)
+def login(request):
+	return render(request,'myapp/login.html')
+
+from django.contrib.auth.models import User
+
+def Register(request):
+	if request.method == 'POST':
+		data = request.POST.copy()
+		first_name = data.get('first_name')
+		last_name = data.get('last_name')
+		email = data.get('email')
+		password = data.get('password')
+		newuser = User()
+		newuser.username = email
+		newuser.email =email
+		newuser.last_name = last_name
+		newuser.set_password()
+		newuser.save()
+	return render(request,'myapp/register.html')
 
 def About(request):
 	return render(request,'myapp/about.html')
